eccc0a5ad02f710ffe31cbdf697a2bf07b96d4d6
Areeb Malik <areebmalik2003@Areebs-MacBook-Pro.local>
2024-06-18 15:13:10 -0400

    first

---
+++ flask_app/config.py
@@ -0,0 +1,3 @@
+# Stores all configuration values
+SECRET_KEY = b'A\xb7,\x98\xa2\x11L\xb7\x95?\x07\xc3U{\xda\x10'
+MONGODB_HOST = 'mongodb+srv://areebmalik2003:Sameareeb03@areeb.maryshp.mongodb.net/reviews?retryWrites=true&w=majority'
